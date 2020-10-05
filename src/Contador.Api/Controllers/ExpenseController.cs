e53472095c5fef522c4d08d34d35145d00a81769
Jakub Ociepa <jakub.ociepa@outlook.com>
2020-10-05 19:23:38 +0200

    Add expenses sample for the Api

---
+++ src/Contador.Api/Controllers/ExpenseController.cs
@@ -0,0 +1,73 @@
+﻿using Contador.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Contador.Api.Controllers
+{
+    /// <summary>
+    /// Expense controller.
+    /// </summary>
+    [ApiController]
+    public class ExpenseController : ControllerBase
+    {
+        /// <summary>
+        /// Gets all available expense.
+        /// </summary>
+        /// <returns>IEnumerable of expenses.</returns>
+        [HttpGet("expenses")]
+        public ActionResult<IEnumerable<Expense>> GetExpenses()
+        {
+            return new[]
+            {
+                new Expense("Marysia", 123, 0,"słodycze"),
+                new Expense("Marysia", 123, 0,"słodycze"),
+                new Expense("Marysia", 123, 0,"słodycze"),
+            };
+        }
+
+        /// <summary>
+        /// Gets expense of provided id.
+        /// </summary>
+        /// <param name="id">Id of the requested expense.</param>
+        /// <returns>Expense of requested id.</returns>
+        [HttpGet("expenses/{id}")]
+        public ActionResult<Expense> GetExpense(int id)
+        {
+            return new Expense("Marysia", 123, 0, "słodycze");
+        }
+
+        /// <summary>
+        /// Creates new expense and add it to db.
+        /// </summary>
+        /// <param name="expense">Expense to add.</param>
+        /// <returns>Http code.</returns>
+        [HttpPost("expenses")]
+        public ActionResult CreateExpense(Expense expense)
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Updates expense of provided id.
+        /// </summary>
+        /// <param name="id">Id of expense to update.</param>
+        /// <param name="expense">Data of expense to update.</param>
+        /// <returns>Http code.</returns>
+        [HttpPut("expense/{id}")]
+        public ActionResult UpdateExpense(int id, Expense expense)
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Removes expense of provided id.
+        /// </summary>
+        /// <param name="id">Id of expense to remove.</param>
+        /// <returns>Http code.</returns>
+        [HttpDelete("expense/{id}")]
+        public ActionResult RemoveExpense(int id)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ src/Contador.Api/Models/Expense.cs
@@ -0,0 +1,44 @@
+﻿namespace Contador.Api.Models
+{
+    /// <summary>
+    /// Expense info.
+    /// </summary>
+    public class Expense
+    {
+        /// <summary>
+        /// The name of the expense e.g. what has been bought.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The name of the <see cref="Category"/> that the expense belongs.
+        /// </summary>
+        public string CategoryName { get; set; }
+
+        /// <summary>
+        /// Id of the <see cref="User"/> which is the creator of this expense.
+        /// </summary>
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// Value of the expense.
+        /// </summary>
+        public decimal Value { get; set; }
+
+        /// <summary>
+        /// Specific description about the expense.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Constructor of the expese.
+        /// </summary>
+        public Expense(string name, decimal value, int userId, string categoryName)
+        {
+            Name = name;
+            Value = value;
+            UserId = userId;
+            CategoryName = categoryName;
+        }
+    }
+}
