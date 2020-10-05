b1f69b02e4ca61c75db98babd4e6f1fd5d9978b2
Jakub Ociepa <jakub.ociepa@outlook.com>
2020-10-05 16:26:49 +0200

    Create user class

---
+++ src/Contador.Core/Models/User.cs
@@ -0,0 +1,25 @@
+﻿namespace Contador.Core.Models
+{
+    /// <summary>
+    /// User info.
+    /// </summary>
+    public class User
+    {
+
+        /// <summary>
+        /// Id of this user.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Name of this user.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Email of this user.
+        /// </summary>
+        public string Email { get; set; }
+
+    }
+}
