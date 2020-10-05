1783b47b5f361957e3be4342bdaca532d609048f
Jakub Ociepa <jakub.ociepa@outlook.com>
2020-10-05 16:22:47 +0200

    Create ExpenseCategory class

---
+++ src/Contador.Core/Models/ExpenseCategory.cs
@@ -0,0 +1,18 @@
+﻿namespace Contador.Core.Models
+{
+    /// <summary>
+    /// The category of the expenses.
+    /// </summary>
+    public class ExpenseCategory
+    {
+        /// <summary>
+        /// The id of this expense category.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The name of the expense category.
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
