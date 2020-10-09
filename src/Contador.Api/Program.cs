98d6d2541d49089819e40e18ec72521dbf0e4acc
Jakub Ociepa <jakub.ociepa@outlook.com>
2020-10-09 18:34:38 +0200

    Change approach with logger

--- src/Contador.Api/Program.cs
+++ src/Contador.Api/Program.cs
@@ -1,5 +1,9 @@
+using System;
+
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+
+using Serilog;
 
 namespace Contador.Api
 {
@@ -7,11 +11,26 @@ namespace Contador.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext().WriteTo.Console().CreateLogger();
+            try
+            {
+                Log.Information("Starting up");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application start-up failed");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
