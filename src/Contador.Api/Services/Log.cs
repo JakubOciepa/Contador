f171155984b8031d3cad1bb61a55e70081603045
Jakub Ociepa <jakub.ociepa@outlook.com>
2020-10-09 18:12:18 +0200

    Add simple logger

---
+++ src/Contador.Api/Services/Log.cs
@@ -0,0 +1,13 @@
+﻿
+using Serilog;
+
+namespace Contador.Api.Services
+{
+    public static class Log
+    {
+        public static readonly ILogger Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.File(@"logs\currentLog.log", rollingInterval: RollingInterval.Month)
+            .CreateLogger();
+    }
+}
