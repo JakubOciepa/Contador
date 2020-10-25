74a78e143137dbdb5a26384a0e2c2425a34526e3
Jakub Ociepa <jakub.ociepa@outlook.com>
2020-10-25 12:30:38 +0100

    Update GetExpenses test

--- src/Contador.Web/Server.Tests/ExpenseServiceTests.cs
+++ src/Contador.Web/Server.Tests/ExpenseServiceTests.cs
@@ -18,11 +18,21 @@ namespace Server.Tests
 {
     public class ExpenseServiceTests
     {
-        private static IList<Contador.DAL.Models.Expense> _DALExpenses = new List<Contador.DAL.Models.Expense>()
+        private static readonly User _expectedUser = new User() { Email = "john@domain.com", Id = 0, Name = "John" };
+        private static readonly ExpenseCategory _expectedCategory = new ExpenseCategory("Słodycze") { Id = 0 };
+
+        private static readonly IList<Contador.DAL.Models.Expense> _returnedExpenses = new List<Contador.DAL.Models.Expense>
         {
             new Contador.DAL.Models.Expense("Słodycze",123,0,1),
             new Contador.DAL.Models.Expense("Słodycze",123,0,1),
             new Contador.DAL.Models.Expense("Słodycze",123,0,1),
+        };
+
+        private static readonly List<Contador.Core.Models.Expense> _expextedExpenses = new List<Contador.Core.Models.Expense>
+        {
+            new Expense("Słodycze", 123 ,_expectedUser, _expectedCategory),
+            new Expense("Słodycze", 123 ,_expectedUser, _expectedCategory),
+            new Expense("Słodycze", 123 ,_expectedUser, _expectedCategory),
         };
 
         [Fact]
@@ -35,12 +45,12 @@ namespace Server.Tests
             var loggerMock = new Mock<ILogger<ExpenseService>>();
 
             expenseRepoMock.Setup(r => r.GetExpenses())
-                .Returns(Task.FromResult(_DALExpenses));
+                .Returns(Task.FromResult(_returnedExpenses));
 
             categoriesRepoMock.Setup(c => c.GetCategoryById(It.IsAny<int>()))
-                .Returns(Task.FromResult(new Result<ExpenseCategory>(ResponseCode.Ok, new ExpenseCategory("Słodycze"))));
+                .Returns(Task.FromResult(new Result<ExpenseCategory>(ResponseCode.Ok, _expectedCategory)));
 
-            usersRepoMock.Setup(u => u.GetUserById(It.IsAny<int>())).Returns(new User() { Name = "John" });
+            usersRepoMock.Setup(u => u.GetUserById(It.IsAny<int>())).Returns(_expectedUser);
 
             var expenseService = new ExpenseService(expenseRepoMock.Object, categoriesRepoMock.Object,
                                         usersRepoMock.Object, loggerMock.Object);
@@ -50,7 +60,8 @@ namespace Server.Tests
 
             //assert
             expenses.ResponseCode.Should().Equals(ResponseCode.Ok);
-            expenses.ReturnedObject.Count.Should().Equals(_DALExpenses.Count);
+            expenses.ReturnedObject.Count.Should().Equals(_returnedExpenses.Count);
+            expenses.ReturnedObject.Should().BeEquivalentTo(_expextedExpenses);
         }
     }
 }
