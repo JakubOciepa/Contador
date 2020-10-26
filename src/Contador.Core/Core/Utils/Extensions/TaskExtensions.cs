f47cf209323059335240dc6c1b09b31b6014ad74
Jakub Ociepa <jakub.ociepa@outlook.com>
2020-10-26 17:32:46 +0100

    Add extension method as shortcut of ConfigureAwait(false)

---
+++ src/Contador.Core/Core/Utils/Extensions/TaskExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contador.Core.Utils.Extensions
+{
+    public static class TaskExtensions
+    {
+        /// <summary>
+        /// Adds ConfigureAwait(false) to <see cref="Task<T>"./>
+        /// </summary>
+        /// <typeparam name="T">Type of task return.</typeparam>
+        /// <param name="task">Target task of this extension.</param>
+        /// <returns><see cref="ConfiguredTaskAwaitable"/> task.</returns>
+        public static ConfiguredTaskAwaitable<T> CAF<T>(this Task<T> task)
+        {
+            return task.ConfigureAwait(false);
+        }
+    }
+}
