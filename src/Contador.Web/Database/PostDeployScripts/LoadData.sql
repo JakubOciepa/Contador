570e4478ababbca112ce55f8d41157d241027880
Jakub Ociepa <jakub.ociepa@outlook.com>
2020-11-03 20:55:43 +0100

    Add stubbed data

--- src/Contador.Web/Database/PostDeployScripts/LoadData.sql
+++ src/Contador.Web/Database/PostDeployScripts/LoadData.sql
@@ -1,2 +1,8 @@
-INSERT INTO Expenses (Name, CategoryId, PersonId, Value, CreateDate, ModifiedDate)
+INSERT INTO Expense (Name, CategoryId, PersonId, Value, CreateDate, ModifiedDate)
 VALUES ("Słodycze", 0, 0, 123.00, CURRENT_DATE(), CURRENT_DATE());
+
+INSERT INTO ExpenseCategory (Name) 
+VALUES ("Slodycze");
+
+INSERT INTO User (Name, Email)
+VALUES ("Marysia", "maria@test.com");
