02001edf7b0b19b58aae62dc67b3655b62bc0eb5
Jakub Ociepa <jakub.ociepa@outlook.com>
2020-11-04 19:25:57 +0100

    Update expense_GetById procedure to get category and user also

--- src/Contador.Web/Database/dbo/StoredProcedures/expense_GetById.sql
+++ src/Contador.Web/Database/dbo/StoredProcedures/expense_GetById.sql
@@ -1,6 +1,10 @@
 CREATE OR REPLACE PROCEDURE expense_GetById
     (expenseId INTEGER)
     BEGIN
-        SELECT * FROM Expense
-        WHERE Id = expenseId;
+        SELECT * FROM Expense ex
+        LEFT JOIN ExpenseCategory ec
+            ON ec.Id = ex.CategoryId
+        LEFT JOIN User us
+            ON us.Id = ex.UserId
+        WHERE ex.Id = expenseId;
     END;
