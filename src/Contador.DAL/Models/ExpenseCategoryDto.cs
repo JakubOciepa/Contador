0d79bbd2162736c7baf202c5170cb00e295002f3
Jakub Ociepa <jakub.ociepa@outlook.com>
2020-11-07 16:40:54 +0100

    Add ParameterName and ProcedureName class to ExpenseCategory class

--- src/Contador.DAL/Models/ExpenseCategoryDto.cs
+++ src/Contador.DAL/Models/ExpenseCategoryDto.cs
@@ -25,5 +25,52 @@ namespace Contador.DAL.Models
         public ExpenseCategoryDto() : base(string.Empty)
         {
         }
+
+        /// <summary>
+        /// Contains names of parameters required by expense category stored procedures.
+        /// </summary>
+        public static class ParameterName
+        {
+            /// <summary>
+            /// Id parameter name.
+            /// </summary>
+            public static readonly string Id = "id_p";
+
+            /// <summary>
+            /// Name parameter name.
+            /// </summary>
+            public static readonly string Name = "name_p";
+        }
+
+        /// <summary>
+        /// Contains names of expense category stored procedures.
+        /// </summary>
+        public static class ProcedureName
+        {
+            /// <summary>
+            /// Name of the procedure which adds new expense category into the database.
+            /// </summary>
+            public static readonly string Add = "expenseCategory_Add";
+
+            /// <summary>
+            /// Name of the procedure which removes expense  category from the database.
+            /// </summary>
+            public static readonly string Delete = "expenseCategory_Delete";
+
+            /// <summary>
+            /// Name of the procedure which gets all available expense categories from the database.
+            /// </summary>
+            public static readonly string GetAll = "expenseCategory_GetAll";
+
+            /// <summary>
+            /// Name of the procedure which gets expense category by id from the database.
+            /// </summary>
+            public static readonly string GetById = "expenseCategory_GetById";
+
+            /// <summary>
+            /// Name of the procedure which updates expense category from the database.
+            /// </summary>
+            public static readonly string Update = "expenseCategory_Update";
+        }
     }
 }
