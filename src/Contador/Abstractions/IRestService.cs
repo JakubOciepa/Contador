3a7a87d0b5eaac6f6d40d3df5ff1822fb773c33f
Jakub Ociepa <jakub.ociepa@outlook.com>
2020-11-10 20:23:24 +0100

    Add IRestService interface

---
+++ src/Contador/Abstractions/IRestService.cs
@@ -0,0 +1,9 @@
+﻿namespace Contador.Abstractions
+{
+    /// <summary>
+    /// Service which consumes REST API requests from Contador REST API.
+    /// </summary>
+    public interface IRestService
+    {
+    }
+}
