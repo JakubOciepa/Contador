5a96f40b93521ea029e844839517614547cfcff1
Jakub Ociepa <jakub.ociepa@outlook.com>
2020-11-11 12:05:34 +0100

    Add method for getting expense by id in RestService

--- src/Contador/Abstractions/IRestService.cs
+++ src/Contador/Abstractions/IRestService.cs
@@ -1,9 +1,20 @@
-﻿namespace Contador.Abstractions
+﻿using System.Threading.Tasks;
+
+using Contador.Core.Common;
+using Contador.Core.Models;
+
+namespace Contador.Abstractions
 {
     /// <summary>
     /// Service which consumes REST API requests from Contador REST API.
     /// </summary>
     public interface IRestService
     {
+        /// <summary>
+        /// Gets expense from API by provided id.
+        /// </summary>
+        /// <param name="id">Id of desired expense.</param>
+        /// <returns>Expense of provided id or <see cref="default"/>.</returns>
+        Task<Result<Expense>> GetExpenseById(int id);
     }
 }
--- src/Contador/Contador/Services/RestService.cs
+++ src/Contador/Contador/Services/RestService.cs
@@ -1,9 +1,56 @@
-﻿using Contador.Abstractions;
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Contador.Abstractions;
+using Contador.Core.Common;
+using Contador.Core.Models;
+using Contador.Core.Utils.Extensions;
+
+using Microsoft.Extensions.Logging;
+
+using Newtonsoft.Json;
 
 namespace Contador.Services
 {
     /// <inheritdoc/>
     public class RestService : IRestService
     {
+
+        private const string REST_ADDR = "http://192.168.1.31:5000/api";
+        private const string EXPENSE_ADDR = "/expense";
+
+        private readonly ILogger _logger;
+
+        public RestService(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        /// <inheritdoc/>
+        public async Task<Result<Expense>> GetExpenseById(int id)
+        {
+            using var client = new HttpClient();
+
+            var result = await client.GetAsync($"{REST_ADDR}{EXPENSE_ADDR}").CAF();
+            if(result.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                //TODO: get correct status code from HttpStatusCode.
+
+                return new Result<Expense>(ResponseCode.Error, default);
+            }
+
+            try
+            {
+                var jsonContent = await result.Content.ReadAsStringAsync().CAF();
+
+                return new Result<Expense>(ResponseCode.Ok, JsonConvert.DeserializeObject<Expense>(jsonContent));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogWarning($"Can not convert JSON into Expense.\n {ex.StackTrace}.");
+                return default;
+            }
+        }
     }
 }
