9642916aced0b4a8197b1ca12bad0bf5acab2729
Jakub Ociepa <jakub.ociepa@outlook.com>
2020-11-12 19:59:30 +0100

    Add ILog interface

---
+++ src/Contador/Abstractions/ILog.cs
@@ -0,0 +1,18 @@
+﻿
+using Contador.Core.Common;
+
+namespace Contador.Abstractions
+{
+    /// <summary>
+    /// Provides basic logging functionality.
+    /// </summary>
+    public interface ILog
+    {
+        /// <summary>
+        /// Writes message of specific type into target output.
+        /// </summary>
+        /// <param name="code">Log level of the message.</param>
+        /// <param name="message">Message to write out.</param>
+        public void Write(LogLevel code, string message);
+    }
+}
