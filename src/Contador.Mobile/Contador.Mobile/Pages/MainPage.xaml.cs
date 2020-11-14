fa5176101cdc98e6c08374546c1448ca4e6ab764
Jakub Ociepa <jakub.ociepa@outlook.com>
2020-11-14 14:41:54 +0100

    Move MainPage to Pages directory and add community toolkit to project

---
+++ src/Contador.Mobile/Contador.Mobile/Pages/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Contador.Abstractions;
+using Contador.Services;
+
+using Xamarin.Forms;
+
+namespace Contador.Mobile.Pages
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
