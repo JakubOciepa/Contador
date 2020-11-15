545a039e10ae8dd3c565190b0fd8ddf63b560224
Jakub Ociepa <jakub.ociepa@outlook.com>
2020-11-15 20:00:03 +0100

    Add icons codes for plus, dollar, user and chart

--- src/Contador.Mobile/Contador.Mobile/Styles/IconFonts.cs
+++ src/Contador.Mobile/Contador.Mobile/Styles/IconFonts.cs
@@ -7,5 +7,9 @@ namespace Contador.Mobile.Styles
     public static class IconFonts
     {
         public const string Home = "\uf015";
+        public const string Dollar = "\uf155";
+        public const string Chart = "\uf080";
+        public const string Plus = "\uf067";
+        public const string User = "\uf007";
     }
 }
