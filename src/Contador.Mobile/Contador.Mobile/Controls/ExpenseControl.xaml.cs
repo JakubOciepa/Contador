776fc7b400d161ad50ea01f15c3f70fe1a1f91d4
Jakub Ociepa <jakub.ociepa@outlook.com>
2020-12-06 14:44:07 +0100

    Small refactor of the rotation animation

--- src/Contador.Mobile/Contador.Mobile/Controls/ExpenseControl.xaml.cs
+++ src/Contador.Mobile/Contador.Mobile/Controls/ExpenseControl.xaml.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System.Threading.Tasks;
+
 using Contador.Core.Models;
 
 using Xamarin.Forms;
@@ -24,48 +25,48 @@ namespace Contador.Mobile.Controls
         public ExpenseControl()
         {
             InitializeComponent();
+
         }
 
         private async void TapGestureRecognizer_Tapped(object sender, System.EventArgs e)
         {
+            await SwitchAvatarImage().ConfigureAwait(true);
+        }
+
+        private async Task SwitchAvatarImage()
+        {
             if (_isCollapsed)
             {
                 AvatarImage.RotationY = -270;
-                await CategoryImage.RotateYTo(-90, ANIMATION_LENGTH, Easing.SpringIn);
+
+                await CategoryImage.RotateYTo(-90, ANIMATION_LENGTH, Easing.SpringIn)
+                    .ConfigureAwait(true);
+
                 CategoryImage.IsVisible = false;
                 AvatarImage.IsVisible = true;
-                await AvatarImage.RotateYTo(-360, ANIMATION_LENGTH, Easing.SpringOut);
+
+                await AvatarImage.RotateYTo(-360, ANIMATION_LENGTH, Easing.SpringOut)
+                    .ConfigureAwait(true);
+
                 AvatarImage.RotationY = 0;
                 _isCollapsed = false;
             }
             else
             {
                 CategoryImage.RotationY = -270;
-                await AvatarImage.RotateYTo(-90, ANIMATION_LENGTH, Easing.SpringIn);
+
+                await AvatarImage.RotateYTo(-90, ANIMATION_LENGTH, Easing.SpringIn)
+                    .ConfigureAwait(true);
+
                 AvatarImage.IsVisible = false;
                 CategoryImage.IsVisible = true;
-                await CategoryImage.RotateYTo(-360, ANIMATION_LENGTH, Easing.SpringOut);
+
+                await CategoryImage.RotateYTo(-360, ANIMATION_LENGTH, Easing.SpringOut)
+                    .ConfigureAwait(true);
+
                 CategoryImage.RotationY = 0;
                 _isCollapsed = true;
             }
-
-            //_ = CategoryImage.FadeTo(0, ANIMATION_LENGTH, Easing.Linear);
-            //_ = AvatarImage.FadeTo(100, ANIMATION_LENGTH, Easing.Linear);
-            ////_ = CategoryImage.RotateYTo(180, ANIMATION_LENGTH, Easing.Linear);
-            //await AvatarImage.RotateYTo(180, ANIMATION_LENGTH, Easing.Linear);
-
-            //CategoryImage.IsVisible = false;
-            //AvatarImage.Opacity = 100;
-            //AvatarImage.IsVisible = true;
-
-
-            //var animation = new ParalelAnimation()
-            //{
-            //    {0, 1, new Animation((f) => AvatarImage.Opacity = f, 100, 0, Easing.Linear) },
-            //    {0, 1, new Animation((f) => AvatarImage.RotationY = f, 180, 0, Easing.Linear) },
-            //};
-
-            //animation.Commit(AvatarImage, nameof(TapGestureRecognizer_Tapped), length: ANIMATION_LENGTH, finished: (_, t) => tcs.SetResult(true));
         }
     }
 }
