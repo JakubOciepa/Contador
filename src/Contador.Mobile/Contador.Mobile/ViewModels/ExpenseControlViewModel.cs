062b6b87ee673fed2d9b6c25f7f1cef1f155c71b
Jakub Ociepa <jakub.ociepa@outlook.com>
2021-01-03 20:07:44 +0100

    Add properties for glyphs and value color into ExpenseControlViewModel

--- src/Contador.Mobile/Contador.Mobile/ViewModels/ExpenseControlViewModel.cs
+++ src/Contador.Mobile/Contador.Mobile/ViewModels/ExpenseControlViewModel.cs
@@ -1,10 +1,33 @@
 ﻿using Contador.Core.Models;
+using Contador.Mobile.Styles;
+
+using Xamarin.Forms;
 
 namespace Contador.Mobile.ViewModels
 {
     public class ExpenseControlViewModel : ViewModelBase
     {
         public Expense Expense { get; }
+
+        //this should be taken from some service by the category name?
+        public FontImageSource CategoryGlyph { get; } = new FontImageSource()
+        {
+            Glyph = FontAwesomeIcon.Solid.AppleAlt,
+            Color = Color.White,
+            FontFamily = "FontAwesome",
+            Size = 20
+        };
+
+        //Same as above
+        public FontImageSource UserGlyph { get; } = new FontImageSource()
+        {
+            Glyph = FontAwesomeIcon.Solid.Angry,
+            Color = Color.White,
+            FontFamily = "FontAwesome",
+            Size = 27
+        };
+
+        public Color ExpenseColor { get; } = Color.Red;
 
         public ExpenseControlViewModel(Expense expense)
         {
