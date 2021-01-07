57f820d57b685d3866da8942b5f4f90a134c0fa1
Jakub Ociepa <jakub.ociepa@outlook.com>
2021-01-07 08:34:56 +0100

    Add ExpenseCategoryRepository class

---
+++ src/Contador.DAL/SQLite/Repositories/ExpenseCategoryRepository.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Contador.Core.Models;
+using Contador.DAL.Abstractions;
+
+namespace Contador.DAL.SQLite.Repositories
+{
+    public class ExpenseCategoryRepository : IExpenseCategoryRepository
+    {
+        public Task<ExpenseCategory> AddCategoryAsync(ExpenseCategory expenseCategory)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IList<ExpenseCategory>> GetCategoriesAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<ExpenseCategory> GetCategoryByIdAsync(int categoryId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> RemoveCategoryAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<ExpenseCategory> UpdateCategoryAsync(int id, ExpenseCategory expenseCategory)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
