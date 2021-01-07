ac0e5cae6b9ff1d82f2497c3a770a21bb9c1634f
Jakub Ociepa <jakub.ociepa@outlook.com>
2021-01-07 08:31:02 +0100

    Add dto class for ExpenseCategory

---
+++ src/Contador.DAL/SQLite/Models/ExpenseCategoryDto.cs
@@ -0,0 +1,30 @@
+﻿using SQLite;
+
+namespace Contador.DAL.SQLite.Models
+{
+    /// <summary>
+    /// The category of the expenses.
+    /// </summary>
+    public class ExpenseCategoryDto
+    {
+        /// <summary>
+        /// The id of this expense category.
+        /// </summary>
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The name of the expense category.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Creates instance of the <see cref="ExpenseCategory"/> class.
+        /// </summary>
+        /// <param name="name"></param>
+        public ExpenseCategoryDto(string name)
+        {
+            Name = name;
+        }
+    }
+}
