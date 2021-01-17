94d6e4f7625055241f08b69d4d4ff2a6af15f796
Jakub Ociepa <jakub.ociepa@outlook.com>
2021-01-17 13:53:04 +0100

    [Mobile] Add Expense edit page

--- src/Contador.Mobile/Contador.Mobile/ViewModels/AddingPageViewModel.cs
+++ src/Contador.Mobile/Contador.Mobile/ViewModels/AddingPageViewModel.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using Contador.Mobile.Pages;
 
 using MvvmHelpers;
 
@@ -10,15 +7,14 @@ using Xamarin.Forms;
 
 namespace Contador.Mobile.ViewModels
 {
-    public class AddingPageViewModel : BaseViewModel
-    {
-        public Command AddExpenseCommand { get; set; }
+	public class AddingPageViewModel : BaseViewModel
+	{
+		public Command AddExpenseCommand { get; set; }
 
 		public AddingPageViewModel()
 		{
-			AddExpenseCommand = new Command(async () 
-				=> await Application.Current.MainPage.DisplayAlert("Tap triggered", "Add expense button tapped!", "OK")
-													  .ConfigureAwait(true));
+			AddExpenseCommand = new Command(
+				async () => await Application.Current.MainPage.Navigation.PushAsync(new EditExpensePage()));
 		}
 
 	}
