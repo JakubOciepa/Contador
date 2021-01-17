05858ac39f9b632c93415500bcbac5ef5f5b75f3
Jakub Ociepa <jakub.ociepa@outlook.com>
2021-01-17 12:34:59 +0100

    [Mobile] Add AddingPageViewModel

---
+++ src/Contador.Mobile/Contador.Mobile/ViewModels/AddingPageViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MvvmHelpers;
+
+using Xamarin.Forms;
+
+namespace Contador.Mobile.ViewModels
+{
+    public class AddingPageViewModel : BaseViewModel
+    {
+        public Command AddExpenseCommand { get; set; }
+
+		public AddingPageViewModel()
+		{
+			AddExpenseCommand = new Command(async () 
+				=> await Application.Current.MainPage.DisplayAlert("Tap triggered", "Add expense button tapped!", "OK")
+													  .ConfigureAwait(true));
+		}
+
+	}
+}
