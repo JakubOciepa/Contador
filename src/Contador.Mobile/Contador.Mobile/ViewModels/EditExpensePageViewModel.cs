bcdea5cdbcb9b26c64e96a4868f977377926ed2d
Jakub Ociepa <jakub.ociepa@outlook.com>
2021-01-26 08:27:38 +0100

    [Mobile] Add empty vm for EditExpensePage

---
+++ src/Contador.Mobile/Contador.Mobile/ViewModels/EditExpensePageViewModel.cs
@@ -0,0 +1,19 @@
+﻿
+using Contador.Abstractions;
+
+using MvvmHelpers;
+
+using TinyIoC;
+
+namespace Contador.Mobile.ViewModels
+{
+	public class EditExpensePageViewModel : BaseViewModel
+	{
+		private readonly IExpenseService _expenseService;
+
+		public EditExpensePageViewModel()
+		{
+			_expenseService = TinyIoCContainer.Current.Resolve<IExpenseService>();
+		}
+	}
+}
