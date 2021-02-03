a7cc9e1eac5ed716a0d031b9892745b43ccf9876
Jakub Ociepa <jakub.ociepa@outlook.com>
2021-02-03 07:32:05 +0100

    [Contador] Add new interfaces for Expense category

---
+++ src/Contador/Abstractions/IExpenseCategoryNotifier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Contador.Core.Models;
+
+namespace Contador.Abstractions
+{
+	public interface IExpenseCategoryNotifier
+	{
+		public event EventHandler<ExpenseCategory> CategoryAdded;
+
+		public event EventHandler<ExpenseCategory> CategoryUpdated;
+
+		public event EventHandler<int> CategoryRemoved;
+	}
+}
--- src/Contador/Abstractions/IExpenseCategoryService.cs
+++ src/Contador/Abstractions/IExpenseCategoryService.cs
@@ -29,7 +29,7 @@ namespace Contador.Abstractions
 		/// </summary>
 		/// <param name="category">Expense category to add.</param>
 		/// <returns><see cref="ResponseCode"/> for operation and added category.</returns>
-		Task<Result<ExpenseCategory>> AddExpenseAsync(ExpenseCategory category);
+		Task<Result<ExpenseCategory>> AddExpenseCategoryAsync(ExpenseCategory category);
 
 		/// <summary>
 		/// Updates expense category of provided id.
@@ -37,13 +37,13 @@ namespace Contador.Abstractions
 		/// <param name="id">Id of category to update.</param>
 		/// <param name="category">Category info.</param>
 		/// <returns>Updated category</returns>
-		Task<Result<ExpenseCategory>> UpdateExpenseAsync(int id, ExpenseCategory category);
+		Task<Result<ExpenseCategory>> UpdateExpenseCategoryAsync(int id, ExpenseCategory category);
 
 		/// <summary>
 		/// Removes expense category of provided id.
 		/// </summary>
 		/// <param name="id">Id of category to remove.</param>
 		/// <returns><see cref="ResponseCode"/> of operation.</returns>
-		Task<ResponseCode> RemoveExpenseAsync(int id);
+		Task<ResponseCode> RemoveExpenseCategoryAsync(int id);
 	}
 }
