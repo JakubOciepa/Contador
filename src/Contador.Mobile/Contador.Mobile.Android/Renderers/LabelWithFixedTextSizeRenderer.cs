b841a4147e7266b9ba9854bf4de34688fa9ff8bf
Kuba <jakub.ociepa@outlook.com>
2021-02-09 08:26:29 +0100

    [Mobile, Droid] Rename renderer for fixed label text size and little refactor

---
+++ src/Contador.Mobile/Contador.Mobile.Android/Renderers/LabelWithFixedTextSizeRenderer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.ComponentModel;
+
+using Android.Content;
+using Android.Widget;
+
+using Contador.Mobile.Droid.Renderers;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Xamarin.Forms.Label), typeof(LabelWithFixedTextSizeRenderer))]
+namespace Contador.Mobile.Droid.Renderers
+{
+	/// <summary>
+	/// Renderer for the label text fixed size.
+	/// </summary>
+	public class LabelWithFixedTextSizeRenderer : LabelRenderer
+	{
+		/// <summary>
+		/// Creates instance of the <see cref="LabelWithFixedTextSizeRenderer"/> class.
+		/// </summary>
+		/// <param name="context"></param>
+		public LabelWithFixedTextSizeRenderer(Context context) : base(context)
+		{
+		}
+
+		/// <inheritdoc/>
+		protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Label> e)
+		{
+			base.OnElementChanged(e);
+			SetFontSizeAgain();
+		}
+
+		/// <inheritdoc/>
+		protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			base.OnElementPropertyChanged(sender, e);
+			SetFontSizeAgain();
+		}
+
+		/// <summary>
+		/// Sets the fixed size of the label text.
+		/// </summary>
+		protected void SetFontSizeAgain()
+		{
+			var xfControl = Element;
+
+			if (Control is TextView nativeControl && xfControl is object)
+			{
+				float size = Convert.ToInt64(xfControl.FontSize);
+				nativeControl.SetTextSize(Android.Util.ComplexUnitType.Dip, size);
+			}
+		}
+	}
+}
