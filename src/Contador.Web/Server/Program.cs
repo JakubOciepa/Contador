68476261a49e0fdb583eae6e159bfb320a92271f
Jakub Ociepa <jakub.ociepa@outlook.com>
2021-03-04 12:05:07 +0100

    [Web] Add swagger

--- src/Contador.Web/Server/Program.cs
+++ src/Contador.Web/Server/Program.cs
@@ -1,6 +1,9 @@
-﻿
+﻿using System;
+
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+
+using Serilog;
 
 namespace Contador.Web.Server
 {
@@ -8,7 +11,26 @@ namespace Contador.Web.Server
 	{
 		public static void Main(string[] args)
 		{
-			CreateHostBuilder(args).Build().Run();
+			Log.Logger = new LoggerConfiguration()
+				.Enrich.FromLogContext()
+				.WriteTo.Console()
+				.WriteTo.File(@"logs\log-.log",
+					outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}",
+					rollingInterval: RollingInterval.Month)
+				.CreateLogger();
+			try
+			{
+				Log.Information("Starting up");
+				CreateHostBuilder(args).Build().Run();
+			}
+			catch (Exception ex)
+			{
+				Log.Fatal(ex, "Application start-up failed");
+			}
+			finally
+			{
+				Log.CloseAndFlush();
+			}
 		}
 
 		public static IHostBuilder CreateHostBuilder(string[] args) =>
