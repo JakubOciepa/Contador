871e32c6a8312494b10ec37fd90bfb9d6c9ffd12
Jakub Ociepa <jakub.ociepa@outlook.com>
2021-03-10 08:33:28 +0100

    [Web] Add logger to web client

--- src/Contador.Web/Client/Program.cs
+++ src/Contador.Web/Client/Program.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Contador.Abstractions;
+using Contador.Web.Client.Services;
+using Serilog;
 
 namespace Contador.Web.Client
 {
@@ -14,10 +17,19 @@ namespace Contador.Web.Client
     {
         public static async Task Main(string[] args)
         {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+			Serilog.Log.Logger = new LoggerConfiguration()
+				.Enrich.FromLogContext()
+				.WriteTo.Console()
+				.WriteTo.File(@"logs\log-.log",
+					outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}",
+					rollingInterval: RollingInterval.Month)
+				.CreateLogger();
+
+			var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+			builder.Services.AddSingleton<ILog, Services.Log>();
 
             await builder.Build().RunAsync();
         }
