c59c6a9106c872b7a581430b6d9c97644a52c2e9
Jakub Ociepa <jakub.ociepa@outlook.com>
2021-03-10 08:34:12 +0100

    [Web] Refactor Expenses.razor.cs

--- src/Contador.Web/Client/Pages/Expenses.razor.cs
+++ src/Contador.Web/Client/Pages/Expenses.razor.cs
@@ -8,46 +8,31 @@ using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 
+using Contador.Abstractions;
 using Contador.Core.Models;
 using Contador.Web.Client.Models;
 
 using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
+using Microsoft.JSInterop;
 
 namespace Contador.Web.Client.Pages
 {
 	public partial class Expenses
 	{
-		[Inject]
-		private HttpClient _httpClient { get; set; }
+		[Inject] private HttpClient _httpClient { get; set; }
+		[Inject] private ILog _logger { get; set; }
+		[Inject] private IJSRuntime _jsRuntime { get; set; }
 
 		private IList<Expense> expenses = new List<Expense>();
-		private IList<ExpenseCategory> categories= new List<ExpenseCategory>();
+		private IList<ExpenseCategory> categories = new List<ExpenseCategory>();
 		private ExpenseModel expenseModel = new();
 
 		protected override async Task OnInitializedAsync()
 		{
-			
-				await GetAndSortExpenses();
 
-				categories = await _httpClient.GetFromJsonAsync<IList<ExpenseCategory>>("api/expensecategory");
-		}
-
-		private async Task GetAndSortExpenses()
-		{
-			try
-			{
-				var result = await _httpClient.GetAsync("api/expense");
-
-				if (result.IsSuccessStatusCode && result.StatusCode is not HttpStatusCode.NoContent)
-				{
-					expenses = (await result.Content.ReadFromJsonAsync<IList<Expense>>())
-						.OrderByDescending(e => e.CreateDate).ToList();
-				}
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine(ex);
-			}
+			expenses = await GetAndSortExpenses();
+			categories = await GetCategories();
 		}
 
 		private async void AddNewExpense()
@@ -78,24 +63,55 @@ namespace Contador.Web.Client.Pages
 			{
 				request.Content = new StringContent(JsonSerializer.Serialize(body), Encoding.UTF8, "application/json");
 
-				Console.WriteLine(JsonSerializer.Serialize(body));
-
 				var result = await _httpClient.SendAsync(request);
 
 				if (result is HttpResponseMessage response && response.IsSuccessStatusCode)
 				{
-					expenses = (await _httpClient.GetFromJsonAsync<IList<Expense>>("api/expense"))
-						.OrderByDescending(e => e.CreateDate)
-						.ToList();
+					await GetAndSortExpenses();
 
 					this.StateHasChanged();
+
 				}
 			}
 			catch (Exception ex)
 			{
-				Console.WriteLine(ex);
+				_logger.Write(Core.Common.LogLevel.Error, $"{ex.Message}:\n{ex.StackTrace}");
+				await _jsRuntime.InvokeVoidAsync("alert", "Cannot add expense!");
+			}
+		}
+
+		private async Task<IList<Expense>> GetAndSortExpenses()
+		{
+			try
+			{
+				var result = await _httpClient.GetAsync("api/expense");
+
+				if (result.IsSuccessStatusCode && result.StatusCode is not HttpStatusCode.NoContent)
+				{
+					return (await result.Content.ReadFromJsonAsync<IList<Expense>>())
+						.OrderByDescending(e => e.CreateDate).ToList();
+				}
+
+				return new List<Expense>();
+			}
+			catch (Exception ex)
+			{
+				_logger.Write(Core.Common.LogLevel.Error, $"{ex.Message}:\n{ex.StackTrace}");
+
+				return new List<Expense>();
+			}
+		}
+
+		private async Task<IList<ExpenseCategory>> GetCategories()
+		{
+			var result = await _httpClient.GetAsync("api/expensecategory");
+
+			if (result.IsSuccessStatusCode && result.StatusCode is not HttpStatusCode.NoContent)
+			{
+				return await result.Content.ReadFromJsonAsync<IList<ExpenseCategory>>();
 			}
 
+			return new List<ExpenseCategory>();
 		}
 	}
 }
