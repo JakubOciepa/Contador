44da3c0811de5ec5c9869dac323ef3feb937951e
Jakub Ociepa <jakub.ociepa@outlook.com>
2021-03-14 11:37:49 +0100

    [Web] Replace expense row with razor component

---
+++ src/Contador.Web/Client/Components/ExpenseComponent.razor.cs
@@ -0,0 +1,13 @@
+﻿
+using Contador.Core.Models;
+
+using Microsoft.AspNetCore.Components;
+
+namespace Contador.Web.Client.Components
+{
+	public partial class ExpenseComponent
+	{
+		[Parameter]
+		public Expense Expense { get; set; }
+	}
+}
