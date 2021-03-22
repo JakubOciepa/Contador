deed79e386006e81529e850724f449543148b80f
Jakub Ociepa <jakub.ociepa@outlook.com>
2021-03-22 07:07:30 +0100

    [ContadorAbstractions,ContadorContador] Add comments to the I|ReportService.

--- src/Contador/Abstractions/IReportService.cs
+++ src/Contador/Abstractions/IReportService.cs
@@ -5,8 +5,18 @@ namespace Contador.Abstractions
 {
 	public interface IReportService
 	{
+		/// <summary>
+		/// Generates <see cref="ReportShort"/> for the provided month.
+		/// </summary>
+		/// <param name="month">Month for the report.</param>
+		/// <returns><see cref="ReportShort"/> for the provided month.</returns>
 		ReportShort GetMonthlyShortReport(int month);
 
+		/// <summary>
+		/// Generates <see cref="ReportShort"/> for the provided year.
+		/// </summary>
+		/// <param name="year">Year for the report.</param>
+		/// <returns><see cref="ReportShort"/>for the provided year.</returns>
 		ReportShort GetYearlyShortReport(int year);
 	}
 }
--- src/Contador/Contador/Services/ReportService.cs
+++ src/Contador/Contador/Services/ReportService.cs
@@ -4,12 +4,21 @@ using Contador.Core.Common;
 
 namespace Contador.Services
 {
+	/// <summary>
+	/// Provides methods for the report creation.
+	/// </summary>
 	public class ReportService : IReportService
 	{
 		private readonly IExpenseManager _expenseManager;
 		private readonly IExpenseCategoryManager _expenseCategoryManager;
 		private readonly ILog _logger;
 
+		/// <summary>
+		/// Creates an instance of the <see cref="ReportService"/> class.
+		/// </summary>
+		/// <param name="expenseManager"></param>
+		/// <param name="expenseCategoryManager"></param>
+		/// <param name="logger"></param>
 		public ReportService(IExpenseManager expenseManager, IExpenseCategoryManager expenseCategoryManager, ILog logger)
 		{
 			_expenseManager = expenseManager;
@@ -17,11 +26,21 @@ namespace Contador.Services
 			_logger = logger;
 		}
 
+		/// <summary>
+		/// Generates <see cref="ReportShort"/> for the provided month.
+		/// </summary>
+		/// <param name="month">Month for the report.</param>
+		/// <returns><see cref="ReportShort"/> for the provided month.</returns>
 		public ReportShort GetMonthlyShortReport(int month)
 		{
 			return null;
 		}
 
+		/// <summary>
+		/// Generates <see cref="ReportShort"/> for the provided year.
+		/// </summary>
+		/// <param name="year">Year for the report.</param>
+		/// <returns><see cref="ReportShort"/>for the provided year.</returns>
 		public ReportShort GetYearlyShortReport(int year)
 		{
 			return null;
