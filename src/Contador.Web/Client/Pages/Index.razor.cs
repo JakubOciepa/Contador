ed8b233df0f418544f5ba9107044ff9a82d4c803
Jakub Ociepa <jakub.ociepa@outlook.com>
2021-03-23 20:31:38 +0100

    [Web] Add simple summary as text

--- src/Contador.Web/Client/Pages/Index.razor.cs
+++ src/Contador.Web/Client/Pages/Index.razor.cs
@@ -1,4 +1,10 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+using Contador.Core.Common;
 
 using Microsoft.AspNetCore.Components;
 
@@ -7,6 +13,19 @@ namespace Contador.Web.Client.Pages
 	public partial class Index
 	{
 		[Inject] private HttpClient _httpClient { get; set; }
+
+		public ReportShort Report { get; set; }
+
+		protected override async Task OnInitializedAsync()
+		{
+			var result = await _httpClient.GetAsync($"api/report/short/{DateTime.Now.Year}/{DateTime.Now.Month}");
+
+			if (result.IsSuccessStatusCode && result.StatusCode is not HttpStatusCode.NoContent)
+			{
+				Report = await result.Content.ReadFromJsonAsync<ReportShort>();
+			}
+
+		}
 	}
 }
 
