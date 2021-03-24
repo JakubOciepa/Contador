befa7c86789647bd55b60b060b82f22b6dbd05ef
Jakub Ociepa <jakub.ociepa@outlook.com>
2021-03-24 17:48:34 +0100

    [Web] Add procedure for getting expenses by Year

---
+++ src/Contador.Web/Database/dbo/StoredProcedures/expense_GetByYear.sql
@@ -0,0 +1,8 @@
+DELIMITER $$
+CREATE OR REPLACE PROCEDURE expense_GetByYear (year_p INTEGER) BEGIN
+SELECT *
+FROM Expense ex
+    LEFT JOIN ExpenseCategory ec ON ec.Id = ex.CategoryId
+    LEFT JOIN User us ON us.Id = ex.UserId
+WHERE DATE_FORMAT(ex.CreateDate, '%Y') = year_p;
+END;
