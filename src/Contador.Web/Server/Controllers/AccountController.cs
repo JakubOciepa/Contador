633d951335174e7cbc9e18b06ad132dc5aa10df2
Jakub Ociepa <jakub.ociepa@outlook.com>
2021-04-12 08:34:05 +0200

    [Web] Add comments to the AccountController

--- src/Contador.Web/Server/Controllers/AccountController.cs
+++ src/Contador.Web/Server/Controllers/AccountController.cs
@@ -3,30 +3,44 @@ using System.Threading.Tasks;
 
 using Contador.Web.Identity;
 
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Contador.Web.Server.Controllers
 {
+	/// <summary>
+	/// Provides functions to manage users.
+	/// </summary>
 	[Route("api/[controller]")]
 	[ApiController]
 	public class AccountController : Controller
 	{
 		private readonly UserManager<IdentityUser> _userManager;
 
+		/// <summary>
+		/// Creates instance of the <see cref="AccountController"/> class.
+		/// </summary>
+		/// <param name="userManager">User manager which will handle user creation.</param>
 		public AccountController(UserManager<IdentityUser> userManager)
 		{
 			_userManager = userManager;
 		}
 
+		/// <summary>
+		/// Creates an account for the user by the provided model.
+		/// </summary>
+		/// <param name="model">Model by which the account will be created.</param>
+		/// <returns>Succeeded = true if account is created or false with errors is there was some.</returns>
 		[HttpPost]
+		[ProducesResponseType(StatusCodes.Status200OK)]
 		public async Task<IActionResult> Post([FromBody] RegisterModel model)
 		{
 			var newUser = new IdentityUser { UserName = model.Name, Email = model.Email };
 
 			var result = await _userManager.CreateAsync(newUser, model.Password);
 
-			if (!result.Succeeded)
+			if (result.Succeeded is false)
 			{
 				var errors = result.Errors.Select(x => x.Description);
 
