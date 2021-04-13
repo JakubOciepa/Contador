f3e4412f1e680a08d199dbf875ead77b41f736de
Jakub Ociepa <jakub.ociepa@outlook.com>
2021-04-13 09:01:31 +0200

    [Web] Refactor Auth service

--- src/Contador.Web/Client/Services/AuthService.cs
+++ src/Contador.Web/Client/Services/AuthService.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text;
@@ -16,25 +13,33 @@ using Microsoft.AspNetCore.Components.Authorization;
 
 namespace Contador.Web.Client.Services
 {
-    public class AuthService : IAuthService
-    {
+	/// <summary>
+	/// Provides methods to auth the user.
+	/// </summary>
+	public class AuthService : IAuthService
+	{
 
 		private readonly HttpClient _httpClient;
 		private readonly AuthenticationStateProvider _authenticationStateProvider;
 		private readonly ILocalStorageService _localStorage;
 
-		public AuthService(HttpClient httpClient,
-						   AuthenticationStateProvider authenticationStateProvider,
-						   ILocalStorageService localStorage)
+		public AuthService(HttpClient httpClient, AuthenticationStateProvider authenticationStateProvider,
+			ILocalStorageService localStorage)
 		{
 			_httpClient = httpClient;
 			_authenticationStateProvider = authenticationStateProvider;
 			_localStorage = localStorage;
 		}
 
+		/// <summary>
+		/// Registers the user by the provided model.
+		/// </summary>
+		/// <param name="registerModel">Data of the user to register.</param>
+		/// <returns>Result of the user registration.</returns>
 		public async Task<RegisterResult> Register(RegisterModel registerModel)
 		{
 			var request = new HttpRequestMessage(HttpMethod.Post, "api/account");
+
 			var body = new
 			{
 				name = registerModel.Name,
@@ -50,13 +55,20 @@ namespace Contador.Web.Client.Services
 			return await result.Content.ReadFromJsonAsync<RegisterResult>();
 		}
 
+		/// <summary>
+		/// Log in the user by provided model.
+		/// </summary>
+		/// <param name="loginModel">Data of the user to log in.</param>
+		/// <returns>Result of the log in.</returns>
 		public async Task<LoginResult> Login(LoginModel loginModel)
 		{
 			var loginAsJson = JsonSerializer.Serialize(loginModel);
+
 			var response = await _httpClient.PostAsync("api/Login", new StringContent(loginAsJson, Encoding.UTF8, "application/json"));
+
 			var loginResult = JsonSerializer.Deserialize<LoginResult>(await response.Content.ReadAsStringAsync(), new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
 
-			if (!response.IsSuccessStatusCode)
+			if (response.IsSuccessStatusCode is false)
 			{
 				return loginResult;
 			}
@@ -68,6 +80,9 @@ namespace Contador.Web.Client.Services
 			return loginResult;
 		}
 
+		/// <summary>
+		/// Logout the logged user.
+		/// </summary>
 		public async Task Logout()
 		{
 			await _localStorage.RemoveItemAsync("authToken");
