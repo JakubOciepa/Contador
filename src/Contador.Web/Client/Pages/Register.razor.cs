61a5184d42f09196ec66f696f0b6956be353a090
Jakub Ociepa <jakub.ociepa@outlook.com>
2021-04-13 08:49:28 +0200

    [Web] Separate Register code from view and small refactor.

---
+++ src/Contador.Web/Client/Pages/Register.razor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Contador.Web.Client.Services;
+using Contador.Web.Identity;
+
+using Microsoft.AspNetCore.Components;
+
+namespace Contador.Web.Client.Pages
+{
+	public partial class Register
+	{
+		[Inject] private IAuthService _authService { get; set; }
+		[Inject] private NavigationManager _navigationManager { get; set; }
+
+		private RegisterModel RegisterModel = new RegisterModel();
+		private bool ShowErrors;
+		private IEnumerable<string> Errors;
+
+		private async Task HandleRegistration()
+		{
+			ShowErrors = false;
+
+			var result = await _authService.Register(RegisterModel);
+
+			if (result.Successful)
+			{
+				_navigationManager.NavigateTo("/login");
+			}
+			else
+			{
+				Errors = result.Errors;
+				ShowErrors = true;
+			}
+		}
+
+	}
+}
