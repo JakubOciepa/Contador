02d8703a43b6d32f875e43c18ef1a415e7b1ed00
Jakub Ociepa <jakub.ociepa@outlook.com>
2021-04-15 20:09:29 +0200

    [Web] Refactor ApiAuthenticationStateProvider class

--- src/Contador.Web/Client/Services/ApiAuthenticationStateProvider.cs
+++ src/Contador.Web/Client/Services/ApiAuthenticationStateProvider.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -9,20 +10,35 @@ using System.Threading.Tasks;
 
 using Blazored.LocalStorage;
 
+using Contador.Abstractions;
+
 using Microsoft.AspNetCore.Components.Authorization;
 
 namespace Contador.Web.Client.Services
 {
+	/// <summary>
+	/// Provides methods to authenticate the user.
+	/// </summary>
 	public class ApiAuthenticationStateProvider : AuthenticationStateProvider
 	{
 		private readonly HttpClient _httpClient;
 		private readonly ILocalStorageService _localStorage;
 
+		/// <summary>
+		/// Creates an instance of the <see cref="ApiAuthenticationStateProvider"/> class.
+		/// </summary>
+		/// <param name="httpClient">HTTP client.</param>
+		/// <param name="localStorage">Local storage to store the token.</param>
 		public ApiAuthenticationStateProvider(HttpClient httpClient, ILocalStorageService localStorage)
 		{
 			_httpClient = httpClient;
 			_localStorage = localStorage;
 		}
+
+		/// <summary>
+		/// Gets the state of the authentication of the user, if any.
+		/// </summary>
+		/// <returns>The <see cref="AuthenticationState"/> of the user, if any.</returns>
 		public override async Task<AuthenticationState> GetAuthenticationStateAsync()
 		{
 			var savedToken = await _localStorage.GetItemAsync<string>("authToken");
@@ -34,64 +50,28 @@ namespace Contador.Web.Client.Services
 
 			_httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", savedToken);
 
-			return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(ParseClaimsFromJwt(savedToken), "jwt")));
+			return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(new JwtSecurityToken(savedToken).Claims, "jwt")));
 		}
 
-		public void MarkUserAsAuthenticated(string email)
+		/// <summary>
+		/// Mark the user as authenticated (logged in).
+		/// </summary>
+		/// <param name="name">The user name to mark as authenticated</param>
+		public void MarkUserAsAuthenticated(string name)
 		{
-			var authenticatedUser = new ClaimsPrincipal(new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, email) }, "apiauth"));
+			var authenticatedUser = new ClaimsPrincipal(new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, name) }, "apiauth"));
 			var authState = Task.FromResult(new AuthenticationState(authenticatedUser));
 			NotifyAuthenticationStateChanged(authState);
 		}
 
+		/// <summary>
+		/// Mark the user as logged out from application.
+		/// </summary>
 		public void MarkUserAsLoggedOut()
 		{
 			var anonymousUser = new ClaimsPrincipal(new ClaimsIdentity());
 			var authState = Task.FromResult(new AuthenticationState(anonymousUser));
 			NotifyAuthenticationStateChanged(authState);
-		}
-
-		private IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
-		{
-			var claims = new List<Claim>();
-			var payload = jwt.Split('.')[1];
-			var jsonBytes = ParseBase64WithoutPadding(payload);
-			var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
-
-			keyValuePairs.TryGetValue(ClaimTypes.Role, out object roles);
-
-			if (roles != null)
-			{
-				if (roles.ToString().Trim().StartsWith("["))
-				{
-					var parsedRoles = JsonSerializer.Deserialize<string[]>(roles.ToString());
-
-					foreach (var parsedRole in parsedRoles)
-					{
-						claims.Add(new Claim(ClaimTypes.Role, parsedRole));
-					}
-				}
-				else
-				{
-					claims.Add(new Claim(ClaimTypes.Role, roles.ToString()));
-				}
-
-				keyValuePairs.Remove(ClaimTypes.Role);
-			}
-
-			claims.AddRange(keyValuePairs.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString())));
-
-			return claims;
-		}
-
-		private byte[] ParseBase64WithoutPadding(string base64)
-		{
-			switch (base64.Length % 4)
-			{
-				case 2: base64 += "=="; break;
-				case 3: base64 += "="; break;
-			}
-			return Convert.FromBase64String(base64);
 		}
 	}
 }
