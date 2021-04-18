0f906b282aa676cd239d00ecd55c1083e8049f64
Jakub Ociepa <jakub.ociepa@outlook.com>
2021-04-18 10:33:22 +0200

    [Web] Add skeleton of the api

---
+++ src/Contador.Web/Server/Controllers/CategoryController.cs
@@ -0,0 +1,50 @@
+﻿
+using System.Threading.Tasks;
+
+using Contador.Core.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Contador.Web.Server.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class CategoryController : Controller
+	{
+		[HttpGet]
+		public async Task<ActionResult<ExpenseCategory>> GetAll()
+		{
+			return Ok();
+		}
+
+		[HttpGet("{id}")]
+		public async Task<ActionResult<ExpenseCategory>> GetById([FromRoute] int id)
+		{
+			return Ok();
+		}
+
+		[HttpGet("{name}")]
+		public async Task<ActionResult<ExpenseCategory>>GetByName([FromRoute] string name)
+		{
+			return Ok();
+		}
+
+		[HttpPost]
+		public async Task<ActionResult> Add([FromBody] ExpenseCategory category)
+		{
+			return Ok();
+		}
+
+		[HttpPut("{id}")]
+		public async Task<ActionResult> Update([FromBody] int id, [FromBody] ExpenseCategory category)
+		{
+			return Ok();
+		}
+
+		[HttpDelete("{id}")]
+		public async Task<ActionResult> Remove([FromBody] int id)
+		{
+			return Ok(); 
+		}
+	}
+}
