a2329b8f28272aa62235be67973bbfc20daf94ff
Jakub Ociepa <jakub.ociepa@outlook.com>
2021-05-17 07:59:39 +0200

    [Web] Update filter procedure

--- src/Contador.Web/Database/dbo/StoredProcedures/expense_GetFiltered.sql
+++ src/Contador.Web/Database/dbo/StoredProcedures/expense_GetFiltered.sql
@@ -4,13 +4,17 @@ CREATE OR REPLACE PROCEDURE expense_GetFiltered (
 		IN categoryName_p VARCHAR(255),
 		IN userName_p VARCHAR(255)
 	) BEGIN
-declare pattern varchar(255);
-set pattern = CONCAT('%', name_p, '%');
+declare namePattern varchar(255);
+declare categoryPattern varchar(255);
+declare userNamePattern varchar(255);
+set namePattern = CONCAT('%', name_p, '%');
+set categoryPattern = CONCAT('%', categoryName_p, '%');
+set userNamePattern = CONCAT('%', userName_p, '%');
 SELECT *
 FROM Expense ex
 	LEFT JOIN ExpenseCategory ec ON ec.Id = ex.CategoryId
 	LEFT JOIN ContadorUsers.AspNetUsers us ON us.Id = ex.UserId
-WHERE ec.Name = categoryName_p
-	AND us.UserName = userName_p
-	AND ex.Name LIKE pattern;
+WHERE ec.Name LIKE categoryPattern
+	AND us.UserName LIKE userNamePattern
+	AND ex.Name LIKE namePattern;
 END;
