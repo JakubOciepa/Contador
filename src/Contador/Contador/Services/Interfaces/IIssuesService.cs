28f8eec84c770112b3966332e7b10e6b2135a2eb
Jakub Ociepa <jakub.ociepa@outlook.com>
2021-05-23 17:23:25 +0200

    [Contador.Contador] Add Issues service definition

--- src/Contador/Contador/Services/Interfaces/IIssuesService.cs
+++ src/Contador/Contador/Services/Interfaces/IIssuesService.cs
@@ -1,7 +1,27 @@
-﻿namespace Contador.Services.Interfaces
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Contador.Core.Common;
+using Contador.Core.Models;
+
+namespace Contador.Services.Interfaces
 {
+	/// <summary>
+	/// Provides mechanism to manage Contador issues.
+	/// </summary>
 	public interface IIssuesService
 	{
+		/// <summary>
+		/// Gets all created and opened issues.
+		/// </summary>
+		/// <returns></returns>
+		Task<Result<IEnumerable<Issue>>> GetAllIssuesAsync();
 
+		/// <summary>
+		/// Add the Contador issue.
+		/// </summary>
+		/// <param name="issue">Information about issue to add.</param>
+		/// <returns>Added issue.</returns>
+		Task<Result<Issue>> AddIssueAsync(Issue issue);
 	}
 }
--- src/Contador/Contador/Services/IssuesService.cs
+++ src/Contador/Contador/Services/IssuesService.cs
@@ -1,9 +1,22 @@
-﻿using Contador.Services.Interfaces;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Contador.Core.Common;
+using Contador.Core.Models;
+using Contador.Services.Interfaces;
 
 namespace Contador.Services
 {
 	public class IssuesService : IIssuesService
 	{
+		public Task<Result<Issue>> AddIssueAsync(Issue issue)
+		{
+			throw new System.NotImplementedException();
+		}
 
+		public Task<Result<IEnumerable<Issue>>> GetAllIssuesAsync()
+		{
+			throw new System.NotImplementedException();
+		}
 	}
 }
