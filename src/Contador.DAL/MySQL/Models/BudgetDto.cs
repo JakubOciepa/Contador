7e3bea65993176cae2b4bff45e4346752f57ea4c
Jakub Ociepa <jakub.ociepa@outlook.com>
2021-06-13 11:58:18 +0200

    [Core,DAL,Web] Add models for the budgets

---
+++ src/Contador.DAL/MySQL/Models/BudgetDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Contador.DAL.MySQL.Models
+{
+	/// <summary>
+	/// Reflects Budget structure from db.
+	/// </summary>
+	public class BudgetDto
+	{
+		/// <summary>
+		/// Gets or sets Id of the budget.
+		/// </summary>
+		public int Id { get; set; }
+		
+		/// <summary>
+		/// Gets or sets start date of the budget.
+		/// </summary>
+		public DateTime StartDate { get; set; }
+
+		/// <summary>
+		/// Gets or sets the end date of the budget.
+		/// </summary>
+		public DateTime EndDate { get; set; }
+	}
+}
---
+++ src/Contador.DAL/MySQL/Models/CategoryBudgetDto.cs
@@ -0,0 +1,28 @@
+﻿namespace Contador.DAL.MySQL.Models
+{
+	/// <summary>
+	/// Reflects the CategoryBudget structure from db.
+	/// </summary>
+	public class CategoryBudgetDto
+	{
+		/// <summary>
+		/// Gets or sets the Id of the category budget.
+		/// </summary>
+		public int Id { get; set; }
+
+		/// <summary>
+		/// Gets or sets the Id of the main budget.
+		/// </summary>
+		public int BudgetId { get; set; }
+
+		/// <summary>
+		/// Gets or sets the category id of the budget.
+		/// </summary>
+		public int CategoryId { get; set; }
+
+		/// <summary>
+		/// Gets or sets the value of the budget.
+		/// </summary>
+		public decimal Value { get; set; }
+	}
+}
