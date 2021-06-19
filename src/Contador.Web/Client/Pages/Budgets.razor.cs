2f6e948b2bf84c6ef37facd92e948943c68e56bd
Jakub Ociepa <jakub.ociepa@outlook.com>
2021-06-19 12:37:35 +0200

    [Web,Contador.Contador] Addig budgets from the Client.

--- src/Contador.Web/Client/Pages/Budgets.razor.cs
+++ src/Contador.Web/Client/Pages/Budgets.razor.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 using Contador.Core.Models;
@@ -22,11 +24,46 @@ namespace Contador.Web.Client.Pages
 
 		public List<Budget> BudgetList { get; set; } = new List<Budget>();
 
+		public Budget AddBudgetModel { get; set; } = new Budget() { StartDate = DateTime.Now };
+
 		protected override async Task OnInitializedAsync()
 		{
 			await base.OnInitializedAsync();
 
 			BudgetList = await GetBudgets();
+		}
+
+		private async void AddNewBudget()
+		{
+			var request = new HttpRequestMessage(HttpMethod.Post, "api/budget");
+
+			try
+			{
+				AddBudgetModel.EndDate = AddBudgetModel.StartDate
+					.AddDays(DateTime.DaysInMonth(AddBudgetModel.StartDate.Year, AddBudgetModel.StartDate.Month) - 1);
+
+				request.Content = GetHttpStringContent();
+
+				var result = await _httpClient.SendAsync(request);
+
+				if (result is HttpResponseMessage response && response.IsSuccessStatusCode)
+				{
+					BudgetList = await GetBudgets();
+
+					this.StateHasChanged();
+				}
+				else if (result.StatusCode is HttpStatusCode.Conflict
+					|| result.StatusCode is HttpStatusCode.BadRequest)
+				{
+					_logger.Write(Core.Common.LogLevel.Error, "Cannot add budget!");
+					await _jsRuntime.InvokeVoidAsync("alert", "Cannot add budget!");
+				}
+			}
+			catch (Exception ex)
+			{
+				_logger.Write(Core.Common.LogLevel.Error, $"{ex.Message}:\n{ex.StackTrace}");
+				await _jsRuntime.InvokeVoidAsync("alert", "Cannot add budget!");
+			}
 		}
 
 		private async Task<List<Budget>> GetBudgets()
@@ -49,6 +86,17 @@ namespace Contador.Web.Client.Pages
 
 				return new List<Budget>();
 			}
+		}
+
+		private StringContent GetHttpStringContent()
+		{
+			var body = new
+			{
+				StartDate = AddBudgetModel.StartDate,
+				EndDate = AddBudgetModel.EndDate,
+			};
+
+			return new StringContent(JsonSerializer.Serialize(body), Encoding.UTF8, "application/json");
 		}
 	}
 }
